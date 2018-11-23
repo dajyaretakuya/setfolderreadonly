2a2124bcd6ec657da94ec9bad34efd94ebdc9f0b
czion272蔡强 <346356745@qq.com>
2018-11-23 09:52:11 +0800

    file name change

---
+++ main.js
@@ -0,0 +1,10 @@
+var binary = require('node-pre-gyp');
+var path = require('path');
+var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
+var binding = require(binding_path);
+
+var addon = require('./build/release/setFolderReadOnly')
+
+exports.setFolderReadOnly = function(location) {
+  return addon.setFolderreadOnly(location);
+}
