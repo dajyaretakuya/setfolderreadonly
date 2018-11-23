039afdf368962b40ba099692906f79647a505a4d
czion272蔡强 <346356745@qq.com>
2018-11-23 13:07:58 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var path = require('path');
 var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
 var binding = require(binding_path);
 
-var addon = require('./build/release/setFolderReadOnly')
+var addon = require('./lib/setFolderReadOnly')
 
 exports.setFolderReadOnly = function(location) {
   return addon.setFolderreadOnly(location);
